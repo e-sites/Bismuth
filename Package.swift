1f3cf961b96e475771528687da7fb6bb083135b3
Bas van Kuijck <bas@e-sites.nl>
2019-09-25 13:06:40 +0200

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -8,19 +8,20 @@ let package = Package(
         .iOS(.v9),
     ],
     products: [
-        .library(
-            name: "Bismuth",
-            targets: ["Bismuth"]),
-        ],
+      .library(
+          name: "Bismuth",
+          targets: ["Bismuth"]),
+    ],
     dependencies: [],
     targets: [
         .target(
             name: "Bismuth",
             dependencies: []
+        ),
         .testTarget(
             name: "BismuthTests",
             dependencies: ["Bismuth"]
-          )
-      ]
+        )
+    ],
     swiftLanguageVersions: [ .v4, .v5 ]
 )
